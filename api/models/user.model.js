31fa3559bb972789a95d0450be2c4f5f29317e61
Nimnapathum <pathumeshan123@gmail.com>
2024-08-13 01:31:15 +0530

    complete google auth functionalitiy

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -15,6 +15,10 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
+    avatar:{
+        type: String,
+        default: "https://st.depositphotos.com/1537427/3571/v/950/depositphotos_35717211-stock-illustration-vector-user-icon.jpg"
+    }
 }, { timestamps: true});
 
 const User = mongoose.model('User' , userSchema);
